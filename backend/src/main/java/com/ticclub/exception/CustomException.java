ef51380580b68baff1f1095313539018414aa509
ManasviWaghmare <160133779+ManasviWaghmare@users.noreply.github.com>
2026-03-06 14:23:47 +0530

    Revert "Frontend"

--- backend/src/main/java/com/ticclub/exception/CustomException.java
+++
@@ -1,7 +0,0 @@
-package com.ticclub.exception;
-
-public class CustomException extends RuntimeException {
-    public CustomException(String message) {
-        super(message);
-    }
-}
--- backend/src/main/java/com/ticclub/repository/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ticclub.repository;
-
-import com.ticclub.model.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
